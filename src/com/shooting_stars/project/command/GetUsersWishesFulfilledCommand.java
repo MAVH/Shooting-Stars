70ef568e872d03e1d85804ba693d869f11538f74
Alexandra597 <sasham597@gmail.com>
2015-04-06 10:14:15 +0300

    Change commands names

--- src/com/shooting_stars/project/command/GetUsersWishesFulfilledCommand.java
+++ src/com/shooting_stars/project/command/GetUsersWishesFulfilledCommand.java
@@ -6,28 +6,13 @@ import com.shooting_stars.project.entity.Wish;
 import com.shooting_stars.project.exception.CommandException;
 import com.shooting_stars.project.exception.LogicException;
 import com.shooting_stars.project.logic.WishLogic;
-import org.apache.struts2.interceptor.SessionAware;
 
 import java.util.ArrayList;
-import java.util.Map;
 
-/**
- * Created by Пользователь on 26.03.2015.
- */
-public class GetMakerWishesCommand extends ActionSupport implements SessionAware {
-    private Map<String, Object> sessionAttributes = null;
+public class GetUsersWishesFulfilledCommand extends ActionSupport {
     private int userId;
     private Exception exception;
     private ArrayList<Wish> wishes;
-    private boolean isPageOwner;
-
-    public boolean getIsPageOwner() {
-        return isPageOwner;
-    }
-
-    public void setPageOwner(boolean isOwner) {
-        this.isPageOwner = isOwner;
-    }
 
     public int getUserId() {
         return userId;
@@ -57,23 +42,12 @@ public class GetMakerWishesCommand extends ActionSupport implements SessionAware
     public String execute() {
         String result = SUCCESS;
         try {
-            User user = (User)sessionAttributes.get("user");
-            if(user.getUserId() == userId) {
-                isPageOwner = true;
-            } else {
-                isPageOwner = false;
-            }
-            System.out.println(isPageOwner);
-            wishes = WishLogic.getWishesByMakerId(userId);
+            wishes = WishLogic.getFulfilledWishesByOwnerId(userId);
         } catch (LogicException e) {
             LOG.error(e.getMessage(), e.getCause());
             exception =  new CommandException(e.getCause());
             result = ERROR;
         }
         return result;
-    }
-    @Override
-    public void setSession(Map<String, Object> stringObjectMap) {
-        this.sessionAttributes = stringObjectMap;
     }
 }
--- src/com/shooting_stars/project/command/GetWishesUserParticipatesInCommand.java
+++ src/com/shooting_stars/project/command/GetWishesUserParticipatesInCommand.java
@@ -6,16 +6,25 @@ import com.shooting_stars.project.entity.Wish;
 import com.shooting_stars.project.exception.CommandException;
 import com.shooting_stars.project.exception.LogicException;
 import com.shooting_stars.project.logic.WishLogic;
+import org.apache.struts2.interceptor.SessionAware;
 
 import java.util.ArrayList;
+import java.util.Map;
 
-/**
- * Created by Пользователь on 26.03.2015.
- */
-public class GetOwnerFulfilledWishesCommand extends ActionSupport {
+public class GetWishesUserParticipatesInCommand extends ActionSupport implements SessionAware {
+    private Map<String, Object> sessionAttributes = null;
     private int userId;
     private Exception exception;
     private ArrayList<Wish> wishes;
+    private boolean isPageOwner;
+
+    public boolean getIsPageOwner() {
+        return isPageOwner;
+    }
+
+    public void setPageOwner(boolean isOwner) {
+        this.isPageOwner = isOwner;
+    }
 
     public int getUserId() {
         return userId;
@@ -45,12 +54,23 @@ public class GetOwnerFulfilledWishesCommand extends ActionSupport {
     public String execute() {
         String result = SUCCESS;
         try {
-            wishes = WishLogic.getFulfilledWishesByOwnerId(userId);
+            User user = (User)sessionAttributes.get("user");
+            if(user.getUserId() == userId) {
+                isPageOwner = true;
+            } else {
+                isPageOwner = false;
+            }
+            System.out.println(isPageOwner);
+            wishes = WishLogic.getWishesByMakerId(userId);
         } catch (LogicException e) {
             LOG.error(e.getMessage(), e.getCause());
             exception =  new CommandException(e.getCause());
             result = ERROR;
         }
         return result;
+    }
+    @Override
+    public void setSession(Map<String, Object> stringObjectMap) {
+        this.sessionAttributes = stringObjectMap;
     }
 }
