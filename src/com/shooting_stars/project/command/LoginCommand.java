9eeb165837f0234d2c397c8ffe39ed393fb3cc0f
veronica948 <veronica948@mail.ru>
2015-05-05 20:03:46 +0300

    Change validation in login

--- src/com/shooting_stars/project/command/LoginCommand.java
+++ src/com/shooting_stars/project/command/LoginCommand.java
@@ -1,15 +1,11 @@
 package com.shooting_stars.project.command;
 
-import com.opensymphony.xwork2.ActionSupport;
 import com.shooting_stars.project.entity.User;
 import com.shooting_stars.project.exception.CommandException;
 import com.shooting_stars.project.exception.LogicException;
 import com.shooting_stars.project.logic.LoginLogic;
 import com.shooting_stars.project.manager.MessageManager;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.struts2.interceptor.SessionAware;
-
-import java.util.Map;
 
 public class LoginCommand extends SessionAwareCommand {
     private String login;
@@ -24,26 +20,17 @@ public class LoginCommand extends SessionAwareCommand {
     @Override
     public void validate() {
         messageManager = (MessageManager)sessionAttributes.get("messageManager");
-        if(StringUtils.isEmpty(login)) {
+        if(StringUtils.isEmpty(login) || StringUtils.isEmpty(password)) {
             addFieldError("login",messageManager.getMessage("message.fields.empty"));
-        }
-        if( StringUtils.isEmpty(password)) {
-            addFieldError("password",messageManager.getMessage("message.fields.empty"));
         }
     }
     @Override
     public String execute() {
         String result;
-        /*
-        if(Validation.isEmpty(login) || Validation.isEmpty(password)) {
-            loginOrPasswordErrorMessage = messageManager.getMessage("message.fields.empty");
-            result  LOGIN;
-        }  else { */
             try {
                 User user = LoginLogic.checkUser(login, password);
                 if (user != null) {
                     sessionAttributes.put(PARAM_CURRENT_USER_ID,user.getUserId());
-                    //sessionAttributes.put("user", user);
                     result = SUCCESS;
                 } else {
                     loginOrPasswordErrorMessage = messageManager.getMessage("message.login.error");
@@ -54,7 +41,6 @@ public class LoginCommand extends SessionAwareCommand {
                 result = ERROR;
                 exception = new CommandException(e.getCause());
             }
-        /*}   */
         return result;
     }
 
