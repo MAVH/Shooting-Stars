722a8116a57b58a69813fda48958b2ff9930b25f
veronica948 <veronica948@mail.ru>
2015-05-12 23:56:50 +0300

    Update changePhoto.jsp

--- web/js/script.js
+++ web/js/script.js
@@ -521,6 +521,17 @@ function updateTime() {
         xmlhttp.send();
     }, 600000);
 }
+function getUploadedPhotoName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }
+    var filename = str.slice(i);
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
 
 
 
